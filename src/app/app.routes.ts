e598356239a031089e2f0c3ad1466bb595b6a854
panichi_niccolo <niccolo.panichi@lascaux.it>
2024-04-28 12:54:08 +0200

    inizializzato il route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import {HomeComponent} from "./pages/home/home.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
+
+//TODO: gestire il redirect alla home-page
