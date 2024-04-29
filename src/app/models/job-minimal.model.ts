b14ac33871b3e2f642c9cda6839da3c8a093d787
panichi_niccolo <niccolo.panichi@lascaux.it>
2024-04-29 09:33:06 +0200

    modificati models da interface a class

--- src/app/models/job-minimal.model.ts
+++ src/app/models/job-minimal.model.ts
@@ -1,7 +1,15 @@
-export interface JobMinimalDTO {
+export class JobMinimalDTO {
   id: number;
   companyName: string;
   title: string;
   companyLogo: string;
   reference: string;
+
+  constructor(data: {id: number, companyName: string, title: string, companyLogo: string, reference: string}) {
+    this.id = data.id;
+    this.companyName = data.companyName;
+    this.title = data.title;
+    this.companyLogo = data.companyLogo;
+    this.reference = data.reference
+  }
 }
--- src/app/models/job.model.ts
+++ src/app/models/job.model.ts
@@ -1,9 +1,18 @@
 import {JobMinimalDTO} from "./job-minimal.model";
 
-export interface JobDTO extends JobMinimalDTO {
+export class JobDTO extends JobMinimalDTO {
   location: string;
   industries: string[];
   types: string[];
   description: string;
   publishDate: Date;
+
+  constructor(data: {id: number, companyName: string, title: string, companyLogo: string, reference: string, location: string, industries: string[], types: string[], description: string, publishDate: Date}) {
+    super(data);
+    this.location = data.location;
+    this.description = data.description;
+    this.publishDate = data.publishDate;
+    this.industries = data.industries;
+    this.types = data.types;
+  }
 }
