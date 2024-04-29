a620846bc0d64f211347f29d3e9eae88453bc188
panichi_niccolo <niccolo.panichi@lascaux.it>
2024-04-29 15:13:21 +0200

    lavorazione del componente favorites

--- src/app/pages/favorites/favorites.component.ts
+++ src/app/pages/favorites/favorites.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {JobMinimalDTO} from "../../models/job-minimal.model";
+import {LocalStorageService} from "../../services/local-storage.service";
+import {JobComponent} from "../../components/job/job.component";
+import {CommonModule} from "@angular/common";
 
 @Component({
   selector: 'app-favorites',
   standalone: true,
-  imports: [],
+  imports: [
+    JobComponent,
+    CommonModule
+  ],
   templateUrl: './favorites.component.html',
   styleUrl: './favorites.component.css'
 })
-export class FavoritesComponent {
+export class FavoritesComponent implements OnInit{
+
+  favoriteJobList: JobMinimalDTO[] | undefined | null;
+
+  constructor(private localStorageService: LocalStorageService) {
+  }
+
+  ngOnInit(): void {
+    this.favoriteJobList = this.localStorageService.getFavoriteJobList();
+  }
 
 }
