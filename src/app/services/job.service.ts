f95a76b693012d3992bedbf7b6201000474215fc
panichi_niccolo <niccolo.panichi@lascaux.it>
2024-04-30 09:42:24 +0200

    aggiunto al servizio di recupero dei job il recupero del dettaglio

--- src/app/services/job.service.ts
+++ src/app/services/job.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {JobMinimalDTO} from "../models/job-minimal.model";
+import {JobDTO} from "../models/job.model";
 
 @Injectable({
   providedIn: 'root'
@@ -10,10 +11,15 @@ export class JobService {
 
   private readonly JOBS_API_PATH: string = 'jobs';
   options = {};
+  //TODO: eliminare options perchè non necessarie
 
   constructor(private httpClient: HttpClient) { }
 
-  getJobList() {
+  getJobList(): Observable<JobMinimalDTO[]> {
     return this.httpClient.get<JobMinimalDTO[]>(this.JOBS_API_PATH, this.options);
+  }
+
+  getJobById(id: number): Observable<JobDTO> {
+    return this.httpClient.get<JobDTO>(this.JOBS_API_PATH + `/${id}`);
   }
 }
