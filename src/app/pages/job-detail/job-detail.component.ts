e8dc8ae4fc8eae7b83d15296f4d860c69f1aaf70
panichi_niccolo <niccolo.panichi@lascaux.it>
2024-04-30 12:28:22 +0200

    created job-detail component

--- src/app/pages/job-detail/job-detail.component.ts
+++ src/app/pages/job-detail/job-detail.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Data, RouterLink} from "@angular/router";
+import {JobDTO} from "../../models/job.model";
+import {CommonModule} from "@angular/common";
 
 @Component({
   selector: 'app-job-detail',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    RouterLink
+  ],
   templateUrl: './job-detail.component.html',
   styleUrl: './job-detail.component.css'
 })
-export class JobDetailComponent {
+export class JobDetailComponent implements OnInit{
+
+  job: JobDTO | undefined;
+
+  constructor(private activatedRoute: ActivatedRoute) {
+  }
+
+  ngOnInit(): void {
+    this.activatedRoute.data.subscribe({
+      next: (res: Data) => {
+        if (res['job']) {
+          this.job = res['job'];
+        }
+      },
+      error: (err) => console.log('Error during retrieving job detail' + err),
+    });
+  }
 
 }
