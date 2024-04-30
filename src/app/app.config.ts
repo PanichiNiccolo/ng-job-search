d9b179136dd878884d19b925b54198e3ea0aab28
panichi_niccolo <niccolo.panichi@lascaux.it>
2024-04-30 12:28:57 +0200

    added logico to scroll up to the page on router navigate

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,23 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {
+  InMemoryScrollingFeature,
+  InMemoryScrollingOptions,
+  provideRouter,
+  withInMemoryScrolling
+} from '@angular/router';
 
 import { routes } from './app.routes';
 import {provideHttpClient} from "@angular/common/http";
 
+/*the Angular Router to always scroll back to the top of the viewport after navigation*/
+const scrollConfig: InMemoryScrollingOptions = {
+  scrollPositionRestoration: 'top',
+  anchorScrolling: 'enabled',
+};
+const inMemoryScrollingFeature: InMemoryScrollingFeature =
+  withInMemoryScrolling(scrollConfig);
+
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient()]
+  providers: [provideRouter(routes, inMemoryScrollingFeature), provideHttpClient()]
 };
