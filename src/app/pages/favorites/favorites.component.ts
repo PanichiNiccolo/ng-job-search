3e8d5e34f917928f8b0365060998246401351bd1
panichi_niccolo <niccolo.panichi@lascaux.it>
2024-04-30 08:40:17 +0200

    gestita la lista dei jobs favorites in modo che non sia null o undefined

--- src/app/pages/favorites/favorites.component.ts
+++ src/app/pages/favorites/favorites.component.ts
@@ -16,7 +16,7 @@ import {CommonModule} from "@angular/common";
 })
 export class FavoritesComponent implements OnInit{
 
-  favoriteJobList: JobMinimalDTO[] | undefined | null;
+  favoriteJobList: JobMinimalDTO[] = [];
 
   constructor(private localStorageService: LocalStorageService) {
   }
