680edba7204f381eab91a2764496e156cda3b143
panichi_niccolo <niccolo.panichi@lascaux.it>
2024-04-30 09:41:34 +0200

    aggiunta la rotta di navigazione per il job-detail

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,21 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
 import {HomeComponent} from "./pages/home/home.component";
 import {FavoritesComponent} from "./pages/favorites/favorites.component";
 import {JobsComponent} from "./pages/jobs/jobs.component";
 import {JobListResolver} from "./resolvers/JobListResolver";
+import {JobResolver} from "./resolvers/JobResolver";
+import {JobDetailComponent} from "./pages/job-detail/job-detail.component";
 
 export const routes: Routes = [
   {
     path: 'home',
     component: HomeComponent,
     children: [
+      {
+        path: 'detail/:id',
+        component: JobDetailComponent,
+        resolve: {job: JobResolver},
+      },
       {
         path: 'jobs',
         component: JobsComponent,
